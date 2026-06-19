b9f99cbeddf0da47bc9860a5311b19df6841a410
Swajith <yadagiri.swajith@klh.edu.in>
2026-06-19 11:44:17 +0530

    Support GitHub Pages and Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from "vite";
+/* import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/STUDENT-DASHBOARD/",
   plugins: [react()],
+}); */
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: process.env.NETLIFY ? "/" : "/STUDENT-DASHBOARD/",
 });
